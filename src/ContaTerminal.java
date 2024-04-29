cae2551caa8fc5cdd03e933f206b77b0fcebd049
Emerson Pessoa <60630092+emersonpessoa01@users.noreply.github.com>
2024-04-29 18:57:02 -0300

    added ContaTerminal :tada:

---
+++ src/ContaTerminal.java
@@ -0,0 +1,29 @@
+import java.util.Locale;
+import java.util.Scanner;
+
+public class ContaTerminal {
+    public static void main(String[] args) throws Exception {
+        @SuppressWarnings("resource")
+        Scanner Scanner = new Scanner(System.in).useLocale(Locale.US);
+
+        System.out.println("Por favor, digite o seu nome: ");
+        String nome = Scanner.next();
+
+        System.out.println("O número de sua agência: ");
+        String agencia = Scanner.next();
+
+        System.out.println("O digite o número de sua conta corrente: ");
+        int conta = Scanner.nextInt();
+
+        System.out.println("Por favor, digite o valor de seu saldo: ");
+        double saldo = Scanner.nextDouble();
+
+        System.err.println("-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=--=-=-=-");
+        System.out.println("Olá " + nome + ",obrigado por criar uma conta em nosso banco!");
+        System.out.println("Sua agência é " + agencia + ",");
+        System.out.println("Conta corrente " + conta + " e seu saldo é " + saldo + ",");
+        System.out.println("Já está disponível para saque.");
+        System.err.println("-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=--=-=-=-");
+
+    }
+}
